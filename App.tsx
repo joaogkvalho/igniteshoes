30f90b716f2cd19679c211a107f7f32aa7e01604
joaogkvalho <joaogkvalho@gmail.com>
2024-07-30 11:19:19 -0300

    project changed to development build

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
-import { StatusBar } from 'react-native';
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
 import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from 'react-native';
+import { OneSignal } from 'react-native-onesignal';
 
 import { Routes } from './src/routes';
 
-import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
+import { THEME } from './src/theme';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.initialize("a66e1ee2-6a71-48ee-9c2d-24e92166ef25")
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
